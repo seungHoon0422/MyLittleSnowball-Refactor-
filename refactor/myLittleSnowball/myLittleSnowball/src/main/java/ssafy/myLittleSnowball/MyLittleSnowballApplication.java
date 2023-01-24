9747070794b3e8b233923b54770d40c508e609c5
seunghoon0422 <psh104404@naver.com>
2023-01-25 06:23:33 +0900

    ADD: MyLittleSnowball Project Setting

---
+++ refactor/myLittleSnowball/myLittleSnowball/src/main/java/ssafy/myLittleSnowball/MyLittleSnowballApplication.java
@@ -0,0 +1,13 @@
+package ssafy.myLittleSnowball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLittleSnowballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLittleSnowballApplication.class, args);
+	}
+
+}
---
+++ refactor/myLittleSnowball/myLittleSnowball/src/test/java/ssafy/myLittleSnowball/MyLittleSnowballApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy.myLittleSnowball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLittleSnowballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
